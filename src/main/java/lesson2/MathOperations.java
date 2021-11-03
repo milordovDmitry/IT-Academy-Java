17f9decd4b413a324ade3be15e7382360bd5a4d2
Dmitry <milordoff@yandex.ru>
2021-11-04 00:55:09 +0300

    Математические операции

--- src/main/java/lesson2/MathOperations.java
+++ src/main/java/lesson2/MathOperations.java
@@ -7,28 +7,26 @@ public class MathOperations {
 
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Сложение");
-        System.out.println("Введите первое число:");
+        System.out.println("Сложение" + "\n" + "Введите первое число:");
         int arg1 = in.nextInt();
         System.out.println("Введите второе число:");
         int arg2 = in.nextInt();
         System.out.println("Сумма чисел равна = " + (arg1 + arg2) + "\n");
 
-        System.out.println("Вычитание");
-        System.out.println("Введите первое число:");
+        System.out.println("Вычитание" + "\n" + "Введите первое число:");
         int arg3 = in.nextInt();
         System.out.println("Введите второе число:");
         int arg4 = in.nextInt();
         System.out.println("Разность чисел равна = " + (arg3 - arg4) + "\n");
 
-        System.out.println("Произведение");
-        System.out.println("Введите первое число:");
+        System.out.println("Произведение" + "\n" + "Введите первое число:");
+
         int arg5 = in.nextInt();
         System.out.println("Введите второе число:");
         int arg6 = in.nextInt();
         System.out.println("Произведение чисел равно = " + (arg5 * arg6) + "\n");
 
-        System.out.println("Деление");
+        System.out.println("Деление чисел");
         System.out.println("Введите первое число:");
         double arg7 = in.nextInt();
         System.out.println("Введите второе число:");
