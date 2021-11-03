2bf52ad1dc7cc8b34f765ef2a9e23f7713a927c3
Dmitry <milordoff@yandex.ru>
2021-11-04 00:44:05 +0300

    Вычисление корней квадратного уравнения

--- src/main/java/lesson2/MathOperations.java
+++ src/main/java/lesson2/MathOperations.java
@@ -5,34 +5,34 @@ import java.util.Scanner;
 public class MathOperations {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-        System.out.println("Сложение" + "\n");
+        System.out.println("Сложение");
         System.out.println("Введите первое число:");
-        int arg1 = scanner.nextInt();
+        int arg1 = in.nextInt();
         System.out.println("Введите второе число:");
-        int arg2 = scanner.nextInt();
+        int arg2 = in.nextInt();
         System.out.println("Сумма чисел равна = " + (arg1 + arg2) + "\n");
 
-        System.out.println("Вычитание" + "\n");
+        System.out.println("Вычитание");
         System.out.println("Введите первое число:");
-        int arg3 = scanner.nextInt();
+        int arg3 = in.nextInt();
         System.out.println("Введите второе число:");
-        int arg4 = scanner.nextInt();
+        int arg4 = in.nextInt();
         System.out.println("Разность чисел равна = " + (arg3 - arg4) + "\n");
 
-        System.out.println("Произведение" + "\n");
+        System.out.println("Произведение");
         System.out.println("Введите первое число:");
-        int arg5 = scanner.nextInt();
+        int arg5 = in.nextInt();
         System.out.println("Введите второе число:");
-        int arg6 = scanner.nextInt();
+        int arg6 = in.nextInt();
         System.out.println("Произведение чисел равно = " + (arg5 * arg6) + "\n");
 
-        System.out.println("Деление" + "\n");
+        System.out.println("Деление");
         System.out.println("Введите первое число:");
-        double arg7 = scanner.nextInt();
+        double arg7 = in.nextInt();
         System.out.println("Введите второе число:");
-        double arg8 = scanner.nextDouble();
+        double arg8 = in.nextDouble();
         System.out.println("Частное чисел равно = " + (arg7 / arg8));
 
 
