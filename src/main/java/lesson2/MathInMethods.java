4f8ee634e449447afd14c080f654b15700851fcd
Dmitry <milordoff@yandex.ru>
2021-11-05 19:00:05 +0300

    Решение математических опеарации через методы

--- src/main/java/lesson2/MathInMethods.java
+++ src/main/java/lesson2/MathInMethods.java
@@ -19,7 +19,6 @@ public class MathInMethods {
                 System.out.println("Сумма чисел " + arg1 + " и " + arg2 + " составляет " + summation(arg1, arg2) +
                         "\n");
 
-
             } else if (command == 2) {
                 System.out.println("Вычитание" + "\n" + "Введите первое число:");
                 int arg1 = in.nextInt();
@@ -27,7 +26,6 @@ public class MathInMethods {
                 int arg2 = in.nextInt();
 
                 System.out.println("Разность чисел " + arg1 + " и " + arg2 + " составляет " + subtraction(arg1, arg2) + "\n");
-
 
             } else if (command == 3) {
                 System.out.println("Умножение" + "\n" + "Введите первое число:");
@@ -37,7 +35,6 @@ public class MathInMethods {
 
                 System.out.println("Произведение чисел " + arg1 + " и " + arg2 + " составляет " + multiplying(arg1,
                         arg2) + "\n");
-
 
             } else if (command == 4) {
                 System.out.println("Деление" + "\n" + "Введите первое число:");
@@ -52,7 +49,6 @@ public class MathInMethods {
                                 arg2) + "\n");
                         break;
                     }
-
                 }
 
             } else if (command == 5) {
@@ -61,9 +57,7 @@ public class MathInMethods {
                 System.out.println("Такой операции нет." + "\n" + "Введите число от 1 до 5.");
             }
         }
-
     }
-
 
     public static void printMenu() {
         System.out.println("Введите номер операции, которую вы хотите совершить");
