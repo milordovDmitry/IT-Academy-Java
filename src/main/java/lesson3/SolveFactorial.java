43d7f5ccd9198a80cae1b7d312c110015c3ed4e5
Dmitry <milordoff@yandex.ru>
2021-11-10 00:42:24 +0300

    домашнее задание (дополнительная часть)

---
+++ src/main/java/lesson3/SolveFactorial.java
@@ -0,0 +1,21 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class SolveFactorial {
+    public static void main(String[] args) {
+        System.out.println("Введите число, для которого хотите посчитать факториал:");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(getFactorial(a));
+    }
+
+    private static long getFactorial(long f) {
+
+        long result = 1;
+        for (long i = f; i > 1; i--) {
+            result = result * i;
+        }
+        return result;
+    }
+}
