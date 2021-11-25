885ef31ccb0a801c03dbfe31d34fa99223149287
Dmitry <milordoff@yandex.ru>
2021-11-25 19:27:04 +0300

    домашнее задание (Вычисление max, min, avg в массивах)

--- src/main/java/lesson6/Array.java
+++ src/main/java/lesson6/Array.java
@@ -13,8 +13,7 @@ public class Array {
             int n = in.nextInt();
             if (n < 1) {
                 System.out.println("Длина массива не может быть меньше 1. Введите длину массива:");
-            }
-            else {
+            } else {
                 int[] arr = createArr(n);
                 System.out.println("Случайно созданный массив: " + Arrays.toString(arr));
                 System.out.println("Минмальное значение в массиве: " + minArray(arr));
@@ -28,38 +27,38 @@ public class Array {
     public static void menu() {
         System.out.println("Введите длину массива: ");
     }
-    public static int[] createArr(int n){
+
+    public static int[] createArr(int n) {
         int[] arr = new int[n];
-        for (int i = 0; i <arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 100);
         }
         return arr;
     }
-    public static int minArray(int arr[]){
+
+    public static int minArray(int[] arr) {
         int min = arr[0];
-        for (int i = 1; i < arr.length; i ++)
+        for (int i = 0; i < arr.length; i++)
             if (min > arr[i]) {
                 min = arr[i];
             }
         return min;
     }
-   public static int maxArray(int arr[]){
-       int max = arr[0];
-       for (int i = 0; i<arr.length; i ++)
-           if (max < arr[i]){
-               max = arr[i];
-           }
-       return max;
-   }
 
-   public static double averArray(int arr[]){
+    public static int maxArray(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++)
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        return max;
+    }
+
+    public static double averArray(int[] arr) {
         double sum = 0;
-       for (int i = 0; i<arr.length; i ++) {
-           sum +=arr[i];
-       }
-       return sum/arr.length;
-
-
-
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum / arr.length;
     }
 }
