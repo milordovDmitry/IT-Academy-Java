8e2b7daf07dcf8250c11b8d1da48addb371ffae8
Dmitry <milordoff@yandex.ru>
2021-12-09 11:30:31 +0300

    домашнее задание. (Конвертер)

--- src/main/java/lesson10/Convertible.java
+++ src/main/java/lesson10/Convertible.java
@@ -3,7 +3,7 @@ package lesson10;
 public interface Convertible {
 
 
-    default double  convert(double value, double ratio) {
-     return value * ratio;
+    default double convert(double value, double ratio) {
+        return value * ratio;
     }
 }
