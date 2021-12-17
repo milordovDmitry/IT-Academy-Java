3f8672b9b57294865cd6b26308573a7f4dc6b07a
Dmitry <milordoff@yandex.ru>
2021-12-17 21:03:22 +0300

    домашнее задание. (Создание исключений)

--- src/main/java/lesson11/Vehicle.java
+++ src/main/java/lesson11/Vehicle.java
@@ -6,8 +6,7 @@ public abstract class Vehicle {
 
     public abstract void kpp(double weightLimit, double widthLimit, double heightLimit);
 
-    public void checkSpd() {
-    }
+    public abstract void checkSpd();
 
     public abstract void checkKpp(double weightLimit, double widthLimit, double heightLimit);
 }
